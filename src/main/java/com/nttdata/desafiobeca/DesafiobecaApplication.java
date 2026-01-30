ad35b975803481df213a3ef58a93eecdbfe49795
Isabella Regina <isabellaregina344@gmail.com>
2026-01-30 15:38:20 -0300

    chore: Configura projeto base

---
+++ src/main/java/com/nttdata/desafiobeca/DesafiobecaApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.desafiobeca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiobecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiobecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/desafiobeca/DesafiobecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.desafiobeca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiobecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
